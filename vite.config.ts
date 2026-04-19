b3c663ded7b366e1ffa5354ef14bd40104744c6a
hayzka <zakiawardhani13@gmail.com>
2026-04-19 19:23:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ViteSingleFile } from "vite-plugin-singlefile";
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
+export default defineConfig({
+  plugins: [react(), ViteSingleFile()],
+  build: {
+    assetsInlineLimit: 100000000, // agar semua assets di-inline ke HTML
+    cssCodeSplit: false           // supaya semua CSS ikut di index.html
+  }
 });
