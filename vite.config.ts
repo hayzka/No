18fd10c1719ce2d7966fab2d5dac0666cb284042
hayzka <zakiawardhani13@gmail.com>
2026-04-19 19:31:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { ViteSingleFile } from "vite-plugin-singlefile";
-
-export default defineConfig({
-  plugins: [react(), ViteSingleFile()],
-  build: {
-    assetsInlineLimit: 100000000, // agar semua assets di-inline ke HTML
-    cssCodeSplit: false           // supaya semua CSS ikut di index.html
-  }
-});
-
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
